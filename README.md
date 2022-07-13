a65f369dda27ee3215b684f0ceb45c98a76cebe8
hetaljoshi2425 <hetaljoshi2425@gmail.com>
2022-07-13 16:43:08 +0530

    exchange rates data based on Euro

--- README.md
+++ README.md
@@ -8,3 +8,9 @@ SELECT SUM(`usersproducts`.`amount` * `usersproducts`.`quantity`) as 'TotalSumma
 SELECT SUM(`usersproducts`.`amount` * `usersproducts`.`quantity`) as 'TotalSummarized', `products`.* FROM `products` INNER JOIN `usersproducts` ON `usersproducts`.`product_id` = `products`.`id` WHERE `products`.`status` = '0'
 //Itemswise Multiplication
 SELECT (`usersproducts`.`amount` * `usersproducts`.`quantity`) as 'TotalSummarized', `usersproducts`.*, `products`.`title`, `products`.`status` FROM `products` INNER JOIN `usersproducts` ON `usersproducts`.`product_id` = `products`.`id` WHERE `products`.`status` = '0' 
+
+
+# 3.8. The exchange rates for USD and RON based on Euro using https://exchangeratesapi.io/ . 
+1) Created account on https://exchangeratesapi.io/ and get access api_key.
+2) Use "Exchange Rates Data API" => https://apilayer.com/marketplace/exchangerates_data-api?preview=true#documentation-tab
+3) Place your access_api_key from your account to this [<access_api_key>] in Usersmanagement controller.
---
+++ application/views/users/exchange_rates.php
@@ -0,0 +1,125 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Welcome to CodeIgniter</title>
+
+	<style type="text/css">
+
+	::selection { background-color: #E13300; color: white; }
+	::-moz-selection { background-color: #E13300; color: white; }
+
+	body {
+		background-color: #fff;
+		margin: 40px;
+		font: 13px/20px normal Helvetica, Arial, sans-serif;
+		color: #4F5155;
+	}
+
+	a {
+		color: #003399;
+		background-color: transparent;
+		font-weight: normal;
+	}
+
+	h1 {
+		color: #444;
+		background-color: transparent;
+		border-bottom: 1px solid #D0D0D0;
+		font-size: 19px;
+		font-weight: normal;
+		margin: 0 0 14px 0;
+		padding: 14px 15px 10px 15px;
+	}
+
+	code {
+		font-family: Consolas, Monaco, Courier New, Courier, monospace;
+		font-size: 12px;
+		background-color: #f9f9f9;
+		border: 1px solid #D0D0D0;
+		color: #002166;
+		display: block;
+		margin: 14px 0 14px 0;
+		padding: 12px 10px 12px 10px;
+	}
+
+	#body {
+		margin: 0 15px 0 15px;
+	}
+
+	p.footer {
+		text-align: right;
+		font-size: 11px;
+		border-top: 1px solid #D0D0D0;
+		line-height: 32px;
+		padding: 0 10px 0 10px;
+		margin: 20px 0 0 0;
+	}
+
+	#container {
+		margin: 10px;
+		border: 1px solid #D0D0D0;
+		box-shadow: 0 0 8px #D0D0D0;
+	}
+	td, th {
+		border: 1px solid #ddd;
+		padding: 8px;
+		}
+	</style>
+</head>
+<body>
+
+<div id="container">
+	<div style="padding:10px;"><b>3.8. The exchange rates for USD and RON based on Euro using https://exchangeratesapi.io/.</b></div>
+    <?php
+    if($response->success == true) {
+        ?>
+        <table width="100%">
+            <thead>
+                <tr>
+                    <th colspan="3">EXCHANGE RATES</th>
+                </tr>
+                <tr>
+                    <th colspan="2">Date : <?=$response->date .' '. date('H:i:s', $response->timestamp)?></th>
+                    <th>Base : <?=$response->base?></th>
+                </tr>
+                <tr>
+                    <th colspan="3">Rates</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                if(count($response->rates)>0) {
+                    $counter = 1;
+                    $exist_array = array('USD','RON');
+                    foreach($response->rates as $ratekey => $ratesval) {
+                        if(in_array($ratekey, $exist_array)) {
+                        ?>
+                        <tr>
+                            <td><?=$counter?></td>
+                            <td><?=$ratekey?></td>
+                            <td><?=$ratesval?></td>
+                        </tr>
+                        <?php
+                        }
+                        $counter++;
+                    }
+                }
+                    
+                ?>
+            </tbody>
+        </table>
+        <?php
+    } else {
+
+    }
+    ?>
+    
+	
+
+</div>
+
+</body>
+</html>
